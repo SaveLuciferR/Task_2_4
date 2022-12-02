9d535f2fd05115b5146f53f9997d81af8f0a775c
SaveLuciferR <cmudrec@mail.ru>
2022-12-02 20:23:25 +0300

    Добавьте файлы проекта.

---
+++ Task_2_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Task_2_4
+{
+	class Programm
+	{
+		static void Main(string[] agrs)
+		{
+			int k = 5;
+
+			for (int i = 6; i <= 10; i++)
+			{
+				for (int j = 0; j < k; j++)
+				{
+					Console.Write(i + "\t");
+				}
+
+				Console.WriteLine();
+				k--;
+			}
+		}
+	}
+}
